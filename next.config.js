620576ec2e94e10a5de45b7764b76ed5f9214b30
shunmasa <masashilandjob@gmail.com>
2020-10-24 22:12:48 +1300

    make it better

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
+const withSass = require('@zeit/next-sass')
+module.exports = withSass({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  }
+})
+
 module.exports = {
+  
   exportPathMap: async function() {
 
     return {
