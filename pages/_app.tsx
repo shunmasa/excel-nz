2a3a71f3ad2bd5ab732369c9e902f16306718440
shunmasa <masashilandjob@gmail.com>
2020-10-30 14:44:11 +1300

    make it better

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,40 @@ import Theme from '../src/ui/Theme';
 import { ToastContainer } from 'react-toastify';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 // import withData from '../src/configureClient';
+import mediaQuery from "css-mediaquery";
 
 import "./styles.scss"
 
 const App = (props:any):JSX.Element => {
 
+  const mobileSsrMatchMedia = query => ({
+    matches: mediaQuery.match(query, {
+      // The estimated CSS width of the browser.
+      width: "0px"
+    })
+  });
+  const desktopSsrMatchMedia = query => ({
+    matches: mediaQuery.match(query, {
+      // The estimated CSS width of the browser.
+      width: "1024px"
+    })
+  });
+  
+  const mobileMuiTheme = createMuiTheme({
+    props: {
+      // Change the default options of useMediaQuery
+      MuiUseMediaQuery: { ssrMatchMedia: mobileSsrMatchMedia }
+    }
+  });
+  const desktopMuiTheme = createMuiTheme({
+    props: {
+      // Change the default options of useMediaQuery
+      MuiUseMediaQuery: { ssrMatchMedia: desktopSsrMatchMedia }
+    }
+  });
   
   useEffect(() => {
    
@@ -29,6 +56,11 @@ const App = (props:any):JSX.Element => {
     const { Component, pageProps} = props;
     return (
       <React.Fragment>
+         <MuiThemeProvider
+        theme={
+          pageProps.deviceType === "mobile" ? mobileMuiTheme : desktopMuiTheme
+        }
+      >
  <ThemeProvider theme={Theme}>
        <CssBaseline />
         <Component {...pageProps} />
@@ -43,6 +75,7 @@ const App = (props:any):JSX.Element => {
           pauseOnHover
         />
  </ThemeProvider>
+ </MuiThemeProvider>
       </React.Fragment>
     );
   }
