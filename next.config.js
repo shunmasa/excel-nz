180983a68784b4271f44f394a415588c71616cb4
shunmasa <masashilandjob@gmail.com>
2020-10-31 16:34:54 +1300

    make it better

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
-const path = require('path');
-const Dotenv = require('dotenv-webpack');
-const dev = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  webpack: (config) => {
-    config.plugins.push(new Dotenv({
-        path: path.join(__dirname, dev ? '.env.development' : '.env.production'),
-        systemvars: true
-    }));
-    config.resolve.extensions = ['.js', '.jsx'];
-    return config
-},
+ 
   exportPathMap: async function() {
  
     return {
