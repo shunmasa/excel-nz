de2e3f48e80c0b603e73f984bddffd6cc33cfe00
shunmasa <masashilandjob@gmail.com>
2020-10-31 16:11:29 +1300

    make it better

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
-
+const path = require('path');
+const dev = process.env.NODE_ENV = 'production';
 module.exports = {
-  
+  webpack: (config) => {
+    config.plugins.push(new Dotenv({
+        path: path.join(__dirname, dev ? '.env.development' : '.env.production'),
+        systemvars: true
+    }));
+    config.resolve.extensions = ['.js', '.jsx'];
+    return config
+},
   exportPathMap: async function() {
-
+ 
     return {
       "/": { page: "/" },
       "/contact": { page: "/contact" },
@@ -12,12 +20,8 @@ module.exports = {
       // "/notice": { page: "/notice" },
     };
   },
-  // experimental: { 
-  //   reactMode: 'concurrent'
-  // },
-  // reactStrictMode: true,
+
   typescript: {
-  
     ignoreBuildErrors: true,
   }
 }
