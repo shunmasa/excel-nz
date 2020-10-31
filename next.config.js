3e337eb45fb23f2bbe31663950410a3dbdb4efbb
shunmasa <masashilandjob@gmail.com>
2020-10-31 15:44:18 +1300

    make it better

--- next.config.js
+++ next.config.js
@@ -1,28 +1,3 @@
-const withSass = require('@zeit/next-sass')
-// const path = require('path');
-
-module.exports = withSass({
-  cssModules: true,
-  cssLoaderOptions: {
-    importLoaders: 1,
-    localIdentName: "[local]___[hash:base64:5]",
-  }
-})
-
-
-// const dev = process.env.NODE_ENV !== 'production';
-// module.exports = {
-//     webpack: (config) => {
-//         config.plugins.push(new Dotenv({
-//             path: path.join(__dirname, dev ? '.env.development' : '.env.production'),
-//             systemvars: true
-//         }));
-//         config.resolve.extensions = ['.js', '.jsx'];
-//         return config
-//     }
-// };
-
-
 
 module.exports = {
   
