f9bb9fb4f8306c370a66f69cac27130e88299c85
shunmasa <masashilandjob@gmail.com>
2020-12-26 15:07:10 +1300

    make it better

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -31,7 +31,7 @@ const authMiddleware = new ApolloLink((operation, forward) => {
 
 const webSocketLink: any = process.browser
   ? new WebSocketLink({
-      uri:"ws://excelnz.herokuapp.com/",
+      uri:"ws://excelnz.herokuapp.com/graphql",
       lazy: true,
       options: {
         reconnect: true,
