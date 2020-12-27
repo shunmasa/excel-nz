394c6877b25721cc841222394e94b34a7ff78c4c
shunmasa <masashilandjob@gmail.com>
2020-12-27 19:34:30 +1300

    make it better

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,3 @@
 // export const SERVER = 'http://localhost:4020/graphql';
-export const SERVER = 'https://excelnz.herokuapp.com/';
+export const SERVER = 'https://excelnz.herokuapp.com/graphql';
 export const WEB_SOCKET_LINK = 'wss://excelnz.herokuapp:4020/graphql';
