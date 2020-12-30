9546145a0fc043289001f10b0638d7ee125b4b57
shunmasa <masashilandjob@gmail.com>
2020-12-30 21:37:52 +1300

    make it better

--- server/config/index.ts
+++ server/config/index.ts
@@ -4,6 +4,6 @@ export default {
    db: 'mongodb+srv://admin:C36143614cc@cluster0-0prmo.mongodb.net/projectX?retryWrites=true&w=majority',
    jwtSecret: 'somesuperkey',
    port: 4020,
-   allowedOrigins: ['http://localhost:3000', 'http://localhost:4020','https://studio.apollographql.com','https://excelnz.herokuapp.com/']
+   allowedOrigins: ['http://localhost:3000', 'http://localhost:4020','https://excelnz.herokuapp.com/','https://excelnz.herokuapp.com/heroku']
 };
 // allowedOrigins: ['http://localhost:3000', 'http://yourapp.com', 'http://localhost:4020']
