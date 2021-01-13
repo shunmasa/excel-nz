c782e671db8dbfae1422dfad51233bb75a72b7d0
shunmasa <masashilandjob@gmail.com>
2021-01-13 18:19:05 +1300

    make it better

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -83,7 +83,7 @@ export const destroyToken = async () => {
 
 const isBrowser = typeof window !== "undefined"
 const httpLink =  createHttpLink({
-  uri: '/graphql', 
+  uri: 'http://localhost:4020/graphql', 
   credentials:  'include', 
   fetch
   // fetch: !isBrowser && fetch,
