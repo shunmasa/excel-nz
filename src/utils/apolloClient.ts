bdeb45ecb7a6021bab82c72f716f18bc68067275
shunmasa <masashilandjob@gmail.com>
2021-01-13 18:43:38 +1300

    make it better

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -83,7 +83,7 @@ export const destroyToken = async () => {
 
 const isBrowser = typeof window !== "undefined"
 const httpLink =  createHttpLink({
-  uri: 'https://excelnz.herokuapp.com', 
+  uri: 'https://localhost:4020/graphql', 
   credentials:  'include', 
   fetch
   // fetch: !isBrowser && fetch,
