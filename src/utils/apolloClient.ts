ecf671b72108f2252ffa1513caeced3921d4fc93
shunmasa <masashilandjob@gmail.com>
2021-01-14 18:11:28 +1300

    make it better

--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 // import { createPersistedQueryLink } from "apollo-link-persisted-queries";
-// import { HttpLink } from 'apollo-link-http';
+import { HttpLink } from 'apollo-link-http';
 import Cookies from 'js-cookie';
 import { split, ApolloLink, concat } from 'apollo-link';
 import { getMainDefinition } from 'apollo-utilities';
@@ -82,7 +82,7 @@ export const destroyToken = async () => {
 };
 
 const isBrowser = typeof window !== "undefined"
-const httpLink =  createHttpLink({
+const httpLink = new HttpLink({
   uri: process.env.GRAPHQL_URI, 
   credentials:  'include', 
   fetch
