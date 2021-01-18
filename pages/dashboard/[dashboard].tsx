b6d5c13fa3c37bbabe757bf1285224629afb4b21
shunmasa <masashilandjob@gmail.com>
2021-01-18 16:51:40 +1300

    make it better

--- pages/dashboard/[dashboard].tsx
+++ pages/dashboard/[dashboard].tsx
@@ -4,7 +4,7 @@ import { Button, LinearProgress} from '@material-ui/core';
 import { TextField } from 'formik-material-ui';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import DropZone from '../../src/components/DropZone'
-import {withApollo} from '../../lib/withApolloData'
+import withApollo from '../../lib/withApollo'
 import { useMutation,useQuery} from '@apollo/react-hooks';
 import UPDATE_POST from '../../src/graphql/mutation/updatePost'
 import { toast } from 'react-toastify';
