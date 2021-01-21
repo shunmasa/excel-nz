fb94acf6bd3f00af51279bc6cb06b3d72cd4240c
shunmasa <masashilandjob@gmail.com>
2021-01-21 13:31:23 +1300

    make it better

--- src/utils/apolloClient.tsx
+++ src/utils/apolloClient.tsx
@@ -65,7 +65,7 @@ export default withApollo(
   
     const httpLink = new HttpLink({
       credentials: 'include',
-      uri: 'https://excel-nz.herokuapp.com/graphql',
+      uri: 'https://excel-nz.herokuapp.com/',
       fetch
     });
     // const webSocketLink: any = process.browser
